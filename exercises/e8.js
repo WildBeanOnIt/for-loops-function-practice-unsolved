7519f56850e092749cb8b1612827afbb7c6e3a2d
Julio Aguilera Jr <julioaguilera337@gmail.com>
2023-04-09 16:08:27 -0500

    problem solved.

--- exercises/e8.js
+++ exercises/e8.js
@@ -1,4 +1,3 @@
-
 // EXERCISE 8
 // Return an array with a bank account object with the greatest balance value
 // Array example: bankAccounts in /data/data.js
@@ -6,10 +5,15 @@
 
 export function getClientWithGreatestBalance(array) {
   // Your code goes here...
+  let highestAccountArr = array[0];
 
+  for (const account of array) {
+    if (account.balance > highestAccountArr.balance) {
+      highestAccountArr = account;
+    }
+  }
+  return [highestAccountArr];
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
