e8549d62029ef104989041556b50afe97360ffa0
Julio Aguilera Jr <julioaguilera337@gmail.com>
2023-04-11 19:46:10 -0500

    problem solved.

--- exercises/e10.js
+++ exercises/e10.js
@@ -5,10 +5,23 @@
 
 export function getClientsWithLetterInName(array, letter) {
   // Your code goes here...
-
+  // const retArr = [];
+  // const letter = ["A", "a", "c", "C", "P", "p"];
+  const retArrNames = [];
+  for (const accounts of array) {
+    const accountsName = accounts.name;
+    for (const char of accountsName) {
+      if (char === letter || char === letter.toUpperCase()) {
+        retArrNames.push(accountsName);
+      }
+    }
+  }
+  return retArrNames;
 }
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-10"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
+
+// test
